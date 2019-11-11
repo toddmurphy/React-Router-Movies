77b1b1c5f9dfb80e2f3910cdfa61b542d00026a7
toddmurphy <toddmurphy93@gmail.com>
2019-11-11 19:50:10 -0400

    updating components and routes for begining of project

--- client/src/App.js
+++ client/src/App.js
@@ -1,18 +1,21 @@
 import React, { useState } from 'react';
-
+import { Route } from 'react-router-dom';
+import Movie from './Movies/Movie';
+import MovieList from './Movies/MovieList';
 import SavedList from './Movies/SavedList';
 
 const App = () => {
-  const [savedList, setSavedList] = useState( [] );
+  const [savedList, setSavedList] = useState([]);
 
   const addToSavedList = movie => {
-    setSavedList( [...savedList, movie] );
+    setSavedList([...savedList, movie]);
   };
 
   return (
     <div>
       <SavedList list={savedList} />
-      <div>Replace this Div with your Routes</div>
+      <Route exact path={'/'} component={MovieList} />
+      <Route exact path={'/movies/2'} component={Movie} />
     </div>
   );
 };
