454cfc59b077ff6c058bdb15c37464f3ef36af83
toddmurphy <toddmurphy93@gmail.com>
2019-11-11 22:42:59 -0400

    completed home redirect link

--- client/src/Movies/SavedList.js
+++ client/src/Movies/SavedList.js
@@ -1,4 +1,6 @@
 import React from 'react';
+import MovieList from './MovieList';
+import { Link } from 'react-router-dom';
 
 const SavedList = props => (
   <div className="saved-list">
@@ -6,7 +8,9 @@ const SavedList = props => (
     {props.list.map(movie => (
       <span className="saved-movie">{movie.title}</span>
     ))}
-    <div className="home-button">Home</div>
+    <Link to={'/'} className="home-button">
+      Home
+    </Link>
   </div>
 );
 
